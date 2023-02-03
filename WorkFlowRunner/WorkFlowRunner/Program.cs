5d1fc661b7a7d693f7558dcd3bb2f9430284d629
Shady <forapps850@gmail.com>
2023-02-03 21:29:55 +0200

    initial robot

---
+++ WorkFlowRunner/WorkFlowRunner/Program.cs
@@ -0,0 +1,39 @@
+﻿using System.Activities;
+using System.Activities.XamlIntegration;
+using System.IO;
+using System.Text;
+
+namespace WorkFlowRunner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filePath = @"E:\CSE\4th comp\grad.Proj\XAMLs\Workflow3.xaml";
+            string tempString = "";
+            StringBuilder xamlWFString = new StringBuilder();
+            StreamReader xamlStreamReader =
+                new StreamReader(filePath);
+            while (tempString != null)
+            {
+                tempString = xamlStreamReader.ReadLine();
+                if (tempString != null)
+                {
+                    xamlWFString.Append(tempString);
+                }
+            }
+            Activity wfInstance = ActivityXamlServices.Load(
+                new StringReader(xamlWFString.ToString()));
+            WorkflowInvoker.Invoke(wfInstance);
+            //try
+            //{
+            //    WorkflowInvoker.Invoke(wfInstance, TimeSpan.FromSeconds(10));
+            //}
+
+            //catch (TimeoutException ex)
+            //{
+            //    Console.WriteLine(ex.Message);
+            //}
+        }
+    }
+}
