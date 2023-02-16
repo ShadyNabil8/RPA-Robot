f78ef11248aaa19a5de423dec9ef4e12e7f03f9b
shady <shadyngheith@gmail.com>
2023-02-16 06:42:11 +0200

    add .NET framework service

---
+++ rpaSlayerRobot/rpaService/RpaRobot.cs
@@ -0,0 +1,48 @@
+﻿using System.ServiceProcess;
+using RobotService;
+using Serilog;
+using System.Timers;
+using System.Threading.Tasks;
+using System;
+using System.Threading;
+
+namespace rpaService
+{
+    public partial class RpaRobot : ServiceBase
+    {
+        private RobotConnection Robot;
+        public RpaRobot()
+        {
+            Log.Logger = new LoggerConfiguration()
+               .WriteTo.File(@"D:\New folder\CSE\grad.Proj\logs\ServiceLog.log")
+               .CreateLogger();
+
+            Log.Information("Service is started");
+
+            Robot = new RobotConnection();
+
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+           // while (true)
+           // {
+                //_logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
+                Log.Information("Service is Sending to Robot");
+                Robot.RobotStartClient();
+                
+           // }
+        }
+
+        protected override void OnStop()
+        {
+            Log.Information("Service is stoped");
+        }
+
+        protected override void OnContinue()
+        {
+            
+        }
+    }
+}
