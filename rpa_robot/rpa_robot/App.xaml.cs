ae662d1511253e5491949af2372f5abc54803fa3
shady <shadyngheith@gmail.com>
2023-07-22 18:55:11 +0300

    add login window

--- rpa_robot/rpa_robot/App.xaml.cs
+++ rpa_robot/rpa_robot/App.xaml.cs
@@ -13,5 +13,16 @@ namespace rpa_robot
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            Login loginWindow = new Login();
+            loginWindow.ShowDialog();
+
+            MainWindow mainWindow = new MainWindow();
+            //mainWindow.Visibility = Visibility.Visible;
+            mainWindow.Activate();
+
+        }
     }
 }
